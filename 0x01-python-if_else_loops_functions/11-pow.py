8dd58229728b3aa20b4cbec5f13995bfdc28cf0c
d12o6aa <doaakarem798@gmail.com>
2023-08-26 16:12:30 -0700

    12

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -4,12 +4,19 @@ def pow(a, b):
     if b == 0:
         return 1
     elif b > 0:
+
         n = 1
         for i in range(b):
             n = a * n
-        return n
+        if a < 0:
+            return n * -1
+        else:
+            return n
     else:
         n = 1
         for i in range(b * -1):
             n = n / a
-        return n
+        if a < 0:
+            return n * -1
+        else:
+            return n
