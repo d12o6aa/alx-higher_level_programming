6794b62dff7a32c2ead605128302f8f9af39b544
d12o6aa <doaakarem798@gmail.com>
2023-08-29 06:09:58 -0700

    I have to use the exception KeyboardInterrupt

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[i]), end="")
             p = 1 + p
-        except:
+        except ValueError:
             continue
     print()
     return p
