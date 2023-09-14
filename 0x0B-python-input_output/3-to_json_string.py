78ac7cfec0bdf3282f610cb79b9798949a6bfbdc
d12o6aa <doaakarem798@gmail.com>
2023-09-14 09:35:34 -0700

    task 3 #1

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import json
+"""3-to_json_string module"""
+
+
+def to_json_string(my_obj):
+    """
+    to_json_string - returns the JSON representation
+    of an object (string).
+    :param my_obj: dictionary
+    :return: JSON representation
+    """
+
+    return json.dumps(my_obj, sort_keys=True)
