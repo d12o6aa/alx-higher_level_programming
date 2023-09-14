cf83ba646e3030e999dbbf64e234c9c158c3a15f
d12o6aa <doaakarem798@gmail.com>
2023-09-14 02:49:36 -0700

    task 0 #3

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+"""
+0-read_file module
+"""
+
+
 def read_file(filename=""):
-    
+    """
+    read_file - reads a text file (UTF8) and prints it to stdout
+    Args:
+        filename: name of the file
+    """
     with open(filename, encoding="utf-8") as myfile:
-        
+
         print(myfile.read())
