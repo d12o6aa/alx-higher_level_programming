bb43821a5a329d589aaf383c83c8bffd32d2cede
d12o6aa <doaakarem798@gmail.com>
2023-09-14 02:52:57 -0700

    task 0 #4

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -12,4 +12,5 @@ def read_file(filename=""):
     """
     with open(filename, encoding="utf-8") as myfile:
 
-        print(myfile.read())
+        for line in myfile:
+            print(line, end="")
