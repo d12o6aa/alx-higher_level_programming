2a49eb384eff5fe9f4c63d744da6eeafdf6cf42c
d12o6aa <doaakarem798@gmail.com>
2023-09-14 04:12:00 -0700

    task_1 #1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+1-write_file module
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    write_file - reads a text file (UTF8) and prints it to stdout
+    Args:
+        filename: name of the file
+        text: the text of file
+    """
+    with open(filename, 'a', encoding="utf-8") as myfile:
+        myfile.write(text)
