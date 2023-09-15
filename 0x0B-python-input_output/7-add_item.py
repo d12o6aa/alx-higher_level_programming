7abe2c9b2bec5d8f8b7591d43b1554056368b998
d12o6aa <doaakarem798@gmail.com>
2023-09-15 06:54:51 -0700

    4

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-"""Module for saving to json"""
-"""os module"""
-import os
-"""7-add_item.py module"""
 
-"""6-load_from_json_file.py module"""
-"""5-save_to_json_file module"""
+"""Module for saving to json"""
+import os
+import sys
+from sys import argv
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
@@ -13,19 +11,9 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 file = "add_item.json"
 list = []
 if os.path.exists(file) == False:
-    
     add = save_to_json_file(list, file)
 
-else:
-    add1 = load_from_json_file(file)
-    for x in add1:
-        list.append(x)
+for x in argv[1:]:
+    list.append(x)
 
-    data = input().split()
-    for x in data:
-        list.append(x)
-
-
-    add = save_to_json_file(list, file)
-
-    add1 = load_from_json_file(file)
+save_to_json_file(list, file)
