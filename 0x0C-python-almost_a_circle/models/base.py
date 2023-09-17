868335f8ec07b371a7c5767b4a98036ebbb2346a
d12o6aa <doaakarem798@gmail.com>
2023-09-17 15:55:12 -0700

    update base file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+import json
+
+
 class Base:
     """
     A representation of the base of our OOP hierarchy.
@@ -14,3 +17,26 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Jsonifies a dictionary so it's quite rightly and longer."""
+        if list_dictionaries is None or not list_dictionaries:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Unjsonifies a dictionary."""
+        if json_string is None or not json_string:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Saves jsonified object to file."""
+        if list_objs is not None:
+            list_objs = [o.to_dictionary() for o in list_objs]
+        with open("{}.json".format(cls.__name__), "w", encoding="utf-8") as f:
+            f.write(cls.to_json_string(list_objs))
