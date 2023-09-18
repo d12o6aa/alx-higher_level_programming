e646361acad55147f007a15b3e9fbbbc956312f8
d12o6aa <doaakarem798@gmail.com>
2023-09-18 11:42:16 -0700

    update docs

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,14 +7,19 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    '''A Square class.'''
+
     def __init__(self, size, x=0, y=0, id=None):
+        '''Constructor.'''
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
+        '''Returns string info about this square.'''
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
 
     @property
     def size(self):
+        '''Size of this square.'''
         return self.width
 
     @size.setter
