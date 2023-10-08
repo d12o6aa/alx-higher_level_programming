c40b5fd38816e0777bf0602c4d3571188f5a31f2
d12o6aa <doaakarem798@gmail.com>
2023-10-08 13:13:19 -0700

    adv #3

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 
-for c in range(ord('a'), ord('z') + 1):
-    print("{}".format(chr(c)), end="")
+a = 122
+while a >= 97:
+    if a % 2 == 0:
+        print("{}".format(chr(a)), end="")
+    else:
+        print("{}".format(chr(a - 32)), end="")
+    a -= 1
