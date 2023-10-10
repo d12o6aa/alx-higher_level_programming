c2c5911d337a8e18cbe402266d789d5c4b0f177b
d12o6aa <doaakarem798@gmail.com>
2023-10-10 14:02:02 -0700

    task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+# !/usr/bin/python3
+
+def is_same_class(obj, a_class):
+    """
+
+    :param obj:
+    :param a_class:
+    :return: True if the object is exactly an instance
+    of the specified class ; otherwise False
+    """
+    return type(obj) == a_class
