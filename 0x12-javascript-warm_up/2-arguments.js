9487815dde1af07309b44003325034d581977909
d12o6aa <doaakarem798@gmail.com>
2023-12-20 22:00:51 +0200

    updaye

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,8 +2,10 @@
 
 const argc = process.argv;
 
-if (argc.length > 2) {
+if (argc.length = 3) {
   console.log('Argument found');
+} else if (argc.length > 3) {
+  console.log('Arguments found');
 } else {
   console.log('No argument');
 }
